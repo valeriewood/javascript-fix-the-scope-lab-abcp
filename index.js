2b2c5d560cb5609eb558782d72bddc202e1520b5
Valerie Wood <valeriewd1@gmail.com>
2018-01-12 00:28:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function myAnimal() {
 
 function yourAnimal() {
 
-  return ('cat\n');
+  return ('dog');
 }
 
 function add2(n) {
   return n + two;
-
+ console.log(const two = 2);
   // Feel free to move things around!
-  const two = 2
+ 
 }
 
 var funkyFunction = function() {
@@ -24,35 +24,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
-
- 1) Fix the Scope yourAnimal() returns your animal:
-
-      Error: Expected 'cat\n' to equal 'cat'
-      + expected - actual
-
-      -cat
-      +cat
-
-      at assert (node_modules/expect/lib/assert.js:29:9)
-      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
-      at Context.<anonymous> (test/index-test.js:12:35)
-
-  2) Fix the Scope add2(n) adds two to n:
-
-      Error: Expected NaN to equal 875
-      + expected - actual
-
-      -NaN
-      +875
-
-      at assert (node_modules/expect/lib/assert.js:29:9)
-      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
-      at Context.<anonymous> (test/index-test.js:23:30)
-
-  3) Fix the Scope theFunk is "FUNKY!":
-     Error: Expected [Function: funkyFunction] to equal 'FUNKY!'
-      at assert (node_modules/expect/lib/assert.js:29:9)
-      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
-      at Context.<anonymous> (test/index-test.js:35:30)
+var theFunk = funkyFunction();
 
